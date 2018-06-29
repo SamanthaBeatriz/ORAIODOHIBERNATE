5af2c5b5bf2470a2716685b371354e5aca0e43a1
Samantha Beatriz <36536979+SamanthaBeatriz@users.noreply.github.com>
2018-06-29 11:27:45 -0300

    fio

---
+++ ProjetoHibernate/src/main/java/view/App.java
@@ -0,0 +1,23 @@
+package view;
+
+import org.hibernate.Session;
+
+import controller.HibernateUtil;
+import model.Alunos;
+
+public class App {
+	
+	 public static void main( String[] args )
+	    {
+	    	Alunos alunos = new Alunos();
+	    	alunos.setNome("Jeferson");
+	    	alunos.setEndereco("Av. Àguia de Háia");
+			
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			session.beginTransaction();
+			session.save(alunos);
+			session.getTransaction().commit();
+
+	    }
+
+}
